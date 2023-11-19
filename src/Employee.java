225ea693907f7b3634f5bafdbdb737c327d852be
rasiksilwal013 <rasiksilwal@gmail.com>
2023-11-18 20:02:18 -0500

    new employee class created -employee

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee{
+    private String name;
+    private int birthYear;
+    private  double monthlyIncome;
+    private double occupationRate;
+
+    //Constructor
+    public Employee(String name, int birthYear, double monthlyIncome, double occupationRate){
+        this.name = name;
+        this.birthYear = birthYear;
+        this.monthlyIncome = monthlyIncome;
+        this.occupationRate=occupationRate;
+
+    }
+
+
+}
