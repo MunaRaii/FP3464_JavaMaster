cf8131c610a1c18b8e25d4dbeee6430935a08568
munarai <munaraii1996@gmail.com>
2023-12-02 11:48:40 -0500

    checking output and modifying code accordingly

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 // Testing the  classes
 public class Main {
     public static void main(String[] args) {
+        // Creating Vehicle instance
+        Car managerCar = new Car("Toyota", "ABC123", "Black", "Sedan", "Automatic", "Sedan");
+        Motorcycle testermotorcycle = new Motorcycle("Harley Davidson", "XYZ897", "Red", "Sport", true);
+        Car programmerCar = new Car("Honda", "DEF456", "White", "SUV", "Manual", "SUV");
 
-
-        Manager manager1 = new Manager("Rasik Silwal", 1998, 5000, 100, 10, 5);
+        // Creating Employee instances
+        Manager manager1 = new Manager("Rasik", 1998, 5000, 100, 10, 5, managerCar);
         manager1.calculateEffectiveOccupationRate(25);
-        Tester tester1 = new Tester("Muna Rai", 1996, 4000, 100, 34);
+        Tester tester1 = new Tester("Muna", 1996, 4000, 100, 34, testermotorcycle);
         tester1.calculateEffectiveOccupationRate(25);
-        Programmer programmer1 = new Programmer("Alishan Gurung", 1999, 6000, 100, 12);
+        Programmer programmer1 = new Programmer("Alishan", 1999, 6000, 100, 12, programmerCar);
         programmer1.calculateEffectiveOccupationRate(25);
 
         // Displaying information about each employee using toString
         System.out.println(manager1);
         System.out.println(tester1);
         System.out.println(programmer1);
+
+        // Displaying annual income for each employee
+        System.out.println("Annual Income for Manager: $" + manager1.annualIncome());
+        System.out.println("Annual Income for Tester: $" + tester1.annualIncome());
+        System.out.println("Annual Income for Programmer: $" + programmer1.annualIncome());
     }
 }
