c1cb9eb8b7f90f77d3491be17bdca798f75b78e2
munarai <munaraii1996@gmail.com>
2023-12-02 08:51:19 -0500

    added annualIncome method

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Car car = new Car("BMW","ABC123", "Black", "Sedan", "Automatic", "Sedan");
-        Motorcycle bike = new Motorcycle("Harley Davidson", "HGJ786", "Black", "Sport", true);
-
-        System.out.println(car);
-        System.out.println(bike);
-    }
-}
